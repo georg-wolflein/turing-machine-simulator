b6107fddc50e8be8e0a16844196005e841d78c44
Georg Wölflein <georgw7777@gmail.com>
2019-02-27 19:48:50 +0000

    Write syntax assertions

--- tm.py
+++ tm.py
@@ -2,10 +2,28 @@ import typing
 import sys
 
 
+class SyntaxError(Exception):
+    def __init__(self, message: str):
+        self.message = message
+
+
+def assert_syntax(cond: bool, message: str):
+    if not cond:
+        raise SyntaxError(message)
+
+
+def assert_cast(var, t: type, message: str):
+    try:
+        return t(var)
+    except ValueError:
+        raise SyntaxError(message)
+
+
 class TuringMachine:
 
     def __init__(self, alphabet: set, states: set, initial: str, accepting: str, rejecting: str):
-        assert accepting != rejecting
+        assert_syntax(accepting != rejecting,
+                      "accept and reject states must be different")
         self.alphabet = alphabet
         self.states = dict({state: dict() for state in states})
         self.initial = initial
@@ -14,17 +32,23 @@ class TuringMachine:
         self.current = self.initial
 
     def add_transition(self, from_state: str, tape_input: str, to_state: str, tape_output: str, move_right: bool):
-        assert from_state in self.states
-        assert to_state in self.states
-        #assert tape_input in self.alphabet
-        #assert tape_output in self.alphabet
-        assert from_state not in (self.accepting, self.rejecting)
-        assert tape_input not in self.states[from_state]
+        assert_syntax(from_state in self.states,
+                      "from state must be a valid state")
+        assert_syntax(to_state in self.states,
+                      "to state must be a valid state")
+        assert_syntax(from_state not in (self.accepting, self.rejecting),
+                      "the accept and reject states may not have outgoing transitions")
+        assert_syntax(
+            tape_input not in self.states[from_state], "transitions cannot be defined multiple times")
+        # assert tape_input in self.alphabet
+        # assert tape_output in self.alphabet
+
         self.states[from_state][tape_input] = (
             to_state, tape_output, move_right)
 
     def process_input(self, input: list):
-        # TODO: assert input in alphabet
+        assert_syntax(all(x in self.alphabet for x in input),
+                      "input contains invalid characters")
         self.tape = input
         self.position = 0
         self.has_accepted = False
@@ -58,7 +82,7 @@ class TuringMachine:
 
     def write(self, letter: str):
         assert self.position >= 0 and self.position < len(self.tape)
-        #assert letter in self.alphabet
+        # assert letter in self.alphabet
         self.tape[self.position] = letter
 
     def move_head(self, right: bool):
@@ -82,8 +106,7 @@ class TuringMachine:
         return self.has_accepted or self.has_rejected
 
 
-if __name__ == "__main__":
-    encoded_machine_file = sys.argv[1]
+def create_turing_machine(encoded_machine_file: str) -> TuringMachine:
     with open(encoded_machine_file, "r") as f:
         states = set()
         initial, accepting, rejecting = None, None, None
@@ -91,39 +114,46 @@ if __name__ == "__main__":
 
         # states heading
         line = f.readline().strip().split(" ")
-        assert len(line) == 2
+        assert_syntax(len(line) == 2, "states heading not properly defined")
         states_str, num_states = line
-        assert states_str == "states"
-        # -- make sure line[1] is int
-        num_states = int(num_states)
+        assert_syntax(states_str == "states",
+                      "expected states heading, got {}".format(states_str))
+        num_states = assert_cast(
+            num_states, int, "the number of states must be an integer")
 
         # process states
         for i in range(num_states):
             line = f.readline().strip().split(" ")
-            assert len(line) <= 2
+            assert_syntax(len(line) <= 2,
+                          "too many arguments to define the state")
             state_name = line[0]
-            assert state_name not in states
+            assert_syntax(state_name not in states,
+                          "state has already been defined")
             states.add(state_name)
             if len(line) == 2:
                 accept_or_reject = line[1]
-                assert accept_or_reject in ("+", "-")
+                assert_syntax(accept_or_reject in ("+", "-"),
+                              "second argument must be + or -")
                 if accept_or_reject == "+":
-                    assert accepting is None
+                    assert_syntax(accepting is None,
+                                  "you cannot define two accepting states")
                     accepting = state_name
                 else:
-                    assert rejecting is None
+                    assert_syntax(rejecting is None,
+                                  "you cannot define two rejecting states")
                     rejecting = state_name
             if i == 0:
                 initial = state_name
 
         # process alphabet
         line = f.readline().strip().split(" ")
-        assert len(line) >= 2
-        assert line[0] == "alphabet"
-        # -- make sure line[1] is int
-        num_letters = int(line[1])
+        assert_syntax(len(line) >= 2, "too few arguments to define alphabet")
+        assert_syntax(line[0] == "alphabet", "expected alphabet heading")
+        num_letters = assert_cast(
+            line[1], int, "the number of letters in the alphabet must be an integer")
         alphabet = line[2::]
-        assert len(alphabet) == num_letters
+        assert_syntax(len(alphabet) == num_letters,
+                      "the actual number of letters needs to match the number of letters provided")
 
         machine = TuringMachine(
             alphabet, states, initial, accepting, rejecting)
@@ -133,15 +163,20 @@ if __name__ == "__main__":
             if not line:
                 break
             line = line.split(" ")
-            assert len(line) == 5
+            assert_syntax(len(line) == 5, "the line requires five arguments")
             from_state, tape_input, to_state, tape_output, action = line
             # assert from_state in states
             # assert to_state in states
             # assert tape_input in alphabet
             # assert tape_output in alphabet
-            assert action in ["L", "R"]
+            assert_syntax(action in ["L", "R"],
+                          "the action must either be L or R")
             move_right = action == "R"
             machine.add_transition(
                 from_state, tape_input, to_state, tape_output, move_right)
+        return machine
 
-        machine.process_input(["a", "a", "a", "b"])
+
+if __name__ == "__main__":
+    machine = create_turing_machine(sys.argv[1])
+    machine.process_input(["a", "a", "a", "b"])
