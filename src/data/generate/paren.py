1ce085718ebbb7b5b51881ddab1e1fcef6380615
Georg Wölflein <georgw7777@gmail.com>
2019-03-05 01:04:06 +0000

    Fixes

--- src/data/generate/paren.py
+++ src/data/generate/paren.py
@@ -7,17 +7,17 @@ from data.generate.util import make_runner
 runner = make_runner("paren.tm")
 
 
-@runner(step=1, trials=1, description=r"the words $(``(\" ``)\")^{\lfloor n/2 \rfloor}$ (prepended by $($ if $n$ is odd) for all $n$")
+@runner(step=1, trials=1, description=r'the words $(``(" ``)")^{\lfloor n/2 \rfloor}$ (prepended by $``("$ if $n$ is odd) for all $n$')
 def pairs(length: int):
     return ["(", ")"] * (length // 2) + [")"] * (length % 2)
 
 
-@runner(step=1, trials=1, description=r"the words $``(\"^{\lceil n/2 \rceil} ``)\"^{\lfloor n/2 \rfloor}$ for all $n$")
+@runner(step=1, trials=1, description=r'the words $``("^{\lceil n/2 \rceil} ``)"^{\lfloor n/2 \rfloor}$ for all $n$')
 def nested(length: int):
     return ["("] * math.ceil(length / 2) + [")"] * (length // 2)
 
 
-@runner(step=1, trials=10, description=r"random words from $L((``(\"+``)\")^*)$ of length $n$")
+@runner(step=1, trials=10, description=r'random words from $L((``("+``)")^*)$ of length $n$')
 def completely_random(length: int):
     return ["(" if random.randint(0, 1) == 0 else ")" for x in range(length)]
 
