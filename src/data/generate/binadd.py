6e1636a1894e79b2d970cc4c36c9f4a6a5640af8
Georg Wölflein <georgw7777@gmail.com>
2019-03-07 09:51:57 +0000

    More data generation

--- src/data/generate/binadd.py
+++ src/data/generate/binadd.py
@@ -1,9 +1,47 @@
 import itertools
 import random
+import math
 from data.generate.util import make_runner
 
 
 runner = make_runner("binadd.tm")
+
+
+def zero_pad(s: str, length: int = 0) -> str:
+    return s + ("0" * (length - len(s)))
+
+
+def int2binstr(i: int) -> str:
+    return "".join(reversed("{:b}".format(i)))
+
+
+def binstr2int(s: str) -> int:
+    return 0 if s == "" else int("".join(reversed(s)), base=2)
+
+
+@runner(step=2, trials=1, description=r"words from $\{1^a \# 1^a \# 0 1^a* \}$ for some $a$")
+def accepting_ones(length: int):
+    if length <= 2:
+        return ["#"] * length
+    lengths = [math.ceil((length - 2) / 3) - 1] * 2
+    values = ["1" * l for l in lengths]
+    values.append(int2binstr(sum([binstr2int(x) for x in values])))
+    lengths.append(length - 2 - sum(lengths))
+    return list("#".join(zero_pad(x, l)
+                         for x, l in zip(values, lengths)))
+
+
+@runner(step=2, trials=10, description=r"random words from $L(\{0+1\}^* \# \{0+1\}^* \# \{0+1\}^*)$ of length $n$ that are accepted")
+def accepting_random(length: int):
+    if length <= 2:
+        return ["#"] * length
+    lengths = [math.ceil((length - 2) / 3) - 1] * 2
+    values = ["".join(str(random.randint(0, 1))
+                      for x in range(l)) for l in lengths]
+    values.append(int2binstr(sum([binstr2int(x) for x in values])))
+    lengths.append(length - 2 - sum(lengths))
+    return list("#".join(zero_pad(x, l)
+                         for x, l in zip(values, lengths)))
 
 
 @runner(step=2, trials=10, description=r"random words from $L((0+1+\#)^*)$ of length $n$")
@@ -23,17 +61,3 @@ def random_three_numbers(length: int):
     t[first_hash] = "#"
     t[second_hash] = "#"
     return t
-
-
-@runner(step=2, trials=10, description=r"random words from $L(\{0+1\}^* \# \{0+1\}^* \# \{0+1\}^*)w$ of length $n$ that are accepted")
-def accepting_three_numbers(length: int):
-    def int2binstr(i: int, length: int) -> str:
-        s = "{:b}".format(i)
-        return s + "".join("0" * (length - len(s)))
-    if length <= 2:
-        return ["#"] * length
-    lengths = random.sample(range(length - 2), 2)
-    values = [random.randint(0, 2 ** l - 1) for l in lengths]
-    lengths.append(length - 2 - sum(lengths))
-    values.append(sum(values))
-    return list("#".join(int2binstr(x, l) for x, l in zip(values, lengths)))
---
+++ src/data/generate/subword.py
@@ -0,0 +1,34 @@
+import itertools
+import random
+import math
+from data.generate.util import make_runner
+
+
+runner = make_runner("subword.tm", "subword_fast")
+
+
+@runner(step=2, trials=1, description=r"words from $\{w\#w \mid \text{$w \in \{0,1\}^{\lfloor n/2 \rfloor\}$")
+def equal(length: int):
+    if length <= 1:
+        return ["#"] * length
+    a = [random.choice(["0", "1"]) for _ in range((length - 1) // 2)]
+    b = a.copy()
+    while len(a) + len(b) + 1 < length:
+        a.append(random.choice(["0", ""]))
+    return a + ["#"] + b
+
+
+@runner(step=2, trials=10, description=r"random words from $L(\{0+1\}^* \# \{0+1\}^*)$ of length $n$ that are accepted")
+def accepting_random(length: int):
+    if length <= 1:
+        return ["#"] * length
+    a = [random.choice(["0", "1"]) for _ in range((length - 1) // 2)]
+    b = [random.choice(["0", "1"]) for _ in range((length - 1) // 2)]
+    while len(a) + len(b) + 1 < length:
+        a.append(random.choice(["0", ""]))
+    return a + ["#"] + b
+
+
+@runner(step=2, trials=10, description=r"random words from $L((0+1+\#)^*)$ of length $n$")
+def completely_random(length: int):
+    return [random.choice(["0", "1", "#"]) for x in range(length)]
